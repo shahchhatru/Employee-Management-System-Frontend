5476c128119db2e2f96da9d86f62618b34bfcef5
shahchhatru <shahkhesavchhatra@gmail.com>
2024-06-25 15:41:32 +0545

    modified code skillLevel to skills

--- src/store/EmployeeStateSlice.ts
+++ src/store/EmployeeStateSlice.ts
@@ -2,50 +2,49 @@ import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { EmployeeInputType } from "../types/employee";
 
 const initialState: EmployeeInputType = {
-    designation: "",
-    salary: 0,
-    joiningDate: "",
-    skillLevel: "",
-    user: ""
-}
+  designation: "",
+  salary: 0,
+  joiningDate: "",
+  skills: "",
+  user: "",
+};
 
 export const employeeStateSlice = createSlice({
-    name: "employeeState",
-    initialState,
-    reducers: {
-        setEmployeeState: (state, action: PayloadAction<EmployeeInputType>) => {
-            state.designation = action.payload.designation;
-            state.salary = action.payload.salary;
-            state.joiningDate = action.payload.joiningDate;
-            state.skillLevel = action.payload.skillLevel;
-            state.user = action.payload.user;
-        },
-        setDesignation: (state, action: PayloadAction<string>) => {
-            state.designation = action.payload;
-        },
-        setSalary: (state, action: PayloadAction<number>) => {
-            state.salary = action.payload;
-        },
-        setJoiningDate: (state, action: PayloadAction<string>) => {
-            state.joiningDate = action.payload;
-        },
-        setSkillLevel: (state, action: PayloadAction<string>) => {
-            state.skillLevel = action.payload;
-        },
-        setUser: (state, action: PayloadAction<string>) => {
-            state.user = action.payload;
-        },
+  name: "employeeState",
+  initialState,
+  reducers: {
+    setEmployeeState: (state, action: PayloadAction<EmployeeInputType>) => {
+      state.designation = action.payload.designation;
+      state.salary = action.payload.salary;
+      state.joiningDate = action.payload.joiningDate;
+      state.skills = action.payload.skills;
+      state.user = action.payload.user;
     },
+    setDesignation: (state, action: PayloadAction<string>) => {
+      state.designation = action.payload;
+    },
+    setSalary: (state, action: PayloadAction<number>) => {
+      state.salary = action.payload;
+    },
+    setJoiningDate: (state, action: PayloadAction<string>) => {
+      state.joiningDate = action.payload;
+    },
+    setskills: (state, action: PayloadAction<string>) => {
+      state.skills = action.payload;
+    },
+    setUser: (state, action: PayloadAction<string>) => {
+      state.user = action.payload;
+    },
+  },
 });
 
 export const {
-    setDesignation,
-    setSalary,
-    setJoiningDate,
-    setSkillLevel,
-    setUser,
-    setEmployeeState
+  setDesignation,
+  setSalary,
+  setJoiningDate,
+  setskills,
+  setUser,
+  setEmployeeState,
 } = employeeStateSlice.actions;
-
 
 export default employeeStateSlice.reducer;
--- src/types/employee.ts
+++ src/types/employee.ts
@@ -1,38 +1,38 @@
 export interface Employee {
-    _id?: string;
-    designation: string;
-    salary: number;
-    joiningDate?: string;
-    skillLevel?: string;
-    user?: User;
+  _id?: string;
+  designation: string;
+  salary: number;
+  joiningDate?: string;
+  skillLevel?: string;
+  user?: User;
 }
 
 export interface User {
-    _id: string;
-    name: string;
-    email: string;
-    role: string;
-    organizationId: string;
-    isVerified: boolean;
+  _id: string;
+  name: string;
+  email: string;
+  role: string;
+  organizationId: string;
+  isVerified: boolean;
 }
 
 export interface EmployeeInputType {
-    designation?: string;
-    salary?: number;
-    joiningDate?: string;
-    skillLevel?: string;
-    user?: string;
+  designation?: string;
+  salary?: number;
+  joiningDate?: string;
+  skills?: string;
+  user?: string;
 }
 
 export interface EmployeeUpdateType {
-    designation: string;
-    salary: number;
-    joiningDate?: string;
-    skillLevel?: string;
-    user?: string;
+  designation: string;
+  salary: number;
+  joiningDate?: string;
+  skills?: string;
+  user?: string;
 }
 export interface EmployeeResponse {
-    status?: string;
-    message: string;
-    data: Employee[];
+  status?: string;
+  message: string;
+  data: Employee[];
 }
