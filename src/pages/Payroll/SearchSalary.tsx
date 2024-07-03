9355672f00cf823c6ef676cefe7110163a66e98e
shahchhatru <shahkhesavchhatra@gmail.com>
2024-07-03 11:05:06 +0545

    salary data table

--- src/pages/Payroll/SearchSalary.tsx
+++ src/pages/Payroll/SearchSalary.tsx
@@ -1,10 +1,10 @@
 import {
-    Dialog,
-    DialogContent,
-    DialogFooter,
-    DialogHeader,
-    DialogTitle,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { useState } from "react";
 import MonthSelect, { Month } from "../components/MonthSelectBox";
@@ -14,51 +14,51 @@ import { Button } from "@/components/ui/button";
 import SalaryDataTableView from "./ModalSalaryDataTable";
 
 interface SalaryDetailsModelProp {
-    userId?: string;
+  userId?: string;
 }
 const SalaryDetailModel = ({ userId }: SalaryDetailsModelProp) => {
-    const [isOpenDetails, setIsOpenDetails] = useState(false);
-    const [month, setMonth] = useState<Month>(Month.JANUARY);
-    const [year, setYear] = useState<string>(new Date().getFullYear().toString());
-    const [user, setUser] = useState<string>(userId ? userId : '');
-    const handleOpenDetails = () => {
-        setIsOpenDetails(true);
-    };
+  const [isOpenDetails, setIsOpenDetails] = useState(false);
+  const [month, setMonth] = useState<Month>(Month.JANUARY);
+  const [year, setYear] = useState<string>(new Date().getFullYear().toString());
+  const [user, setUser] = useState<string>(userId ? userId : "");
+  const handleOpenDetails = () => {
+    setIsOpenDetails(true);
+  };
 
-    const handleCloseDetails = () => {
-        setIsOpenDetails(false);
-    };
-    return (
-        <div className="w-full flex gap-2 justify-center items-center">
-            <div>
-                <MonthSelect value={month} onChange={setMonth} />
-            </div>
-            <div>
-                <YearSelect value={year} onChange={setYear} />
-            </div>
-            <div>
-                <UserSelect defaultValue={user} onChange={setUser} />
-            </div>
-            <div>
-                <Dialog open={isOpenDetails} onOpenChange={setIsOpenDetails}>
-                    <DialogTrigger
-                        className="hover:bg-custom-reddanger rounded p-2"
-                        onClick={handleOpenDetails}
-                    >
-                        Search
-                    </DialogTrigger>
-                    <DialogContent className="sm:max-w-[425px] text-custom-headingText">
-                        <DialogHeader>
-                            <DialogTitle>SalaryDataTable</DialogTitle>
-                        </DialogHeader>
-                        <SalaryDataTableView month={month} year={year} user={user} />
-                        <DialogFooter>
-                            <Button onClick={handleCloseDetails}>Close</Button>
-                        </DialogFooter>
-                    </DialogContent>
-                </Dialog>
-            </div>
-        </div>
-    );
+  const handleCloseDetails = () => {
+    setIsOpenDetails(false);
+  };
+  return (
+    <div className="w-full flex gap-2 justify-center items-center">
+      <div>
+        <MonthSelect value={month} onChange={setMonth} />
+      </div>
+      <div>
+        <YearSelect value={year} onChange={setYear} />
+      </div>
+      <div>
+        <UserSelect defaultValue={user} onChange={setUser} />
+      </div>
+      <div>
+        <Dialog open={isOpenDetails} onOpenChange={setIsOpenDetails}>
+          <DialogTrigger
+            className="hover:bg-custom-reddanger rounded p-2"
+            onClick={handleOpenDetails}
+          >
+            Search
+          </DialogTrigger>
+          <DialogContent className="sm:max-w-full w-full text-custom-headingText">
+            <DialogHeader>
+              <DialogTitle>SalaryDataTable</DialogTitle>
+            </DialogHeader>
+            <SalaryDataTableView month={month} year={year} user={user} />
+            <DialogFooter>
+              <Button onClick={handleCloseDetails}>Close</Button>
+            </DialogFooter>
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
 };
 export default SalaryDetailModel;
